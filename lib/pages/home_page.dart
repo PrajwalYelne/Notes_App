e01247348ec6be17a56df4284e9cf3382d7ac020
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2021-08-30 08:43:36 +0530

    Home Page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,201 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+import '../controllers/note_controller.dart';
+import '../pages/add_new_note_page.dart';
+import '../pages/note_detail_page.dart';
+import '../widgets/alertdialog_widget.dart';
+import '../widgets/search_bar.dart';
+
+class HomePage extends StatelessWidget {
+  final controller = Get.put(NoteController());
+
+  Widget emptyNotes() {
+    return Container(
+
+        child: Align(
+          alignment: Alignment.center,
+                 child: RichText(
+                  text: TextSpan(
+                    text: 'There is no note available\n Tap on',
+
+                    style: TextStyle(color: Colors.grey[500],
+                        fontSize: 20.0), children: <TextSpan>[
+
+                      TextSpan(text: ' + ',
+                          style: TextStyle(color: Colors.blue,
+                              fontSize: 25.0)
+                        //(fontWeight: FontWeight.bold)),
+                      ),
+
+                    TextSpan(text: 'to add new note',
+                        style: TextStyle(color: Colors.grey[500],
+                            fontSize: 20.0)
+                    ),
+
+                   ],
+                  ),
+                 ),
+        ),
+     );
+  }
+
+  Widget viewNotes() {
+    return Scrollbar(
+      child: Container(
+        padding: EdgeInsets.only(
+          top: 10,
+          right: 10,
+          left: 10,
+        ),
+        child: StaggeredGridView.countBuilder(
+          itemCount: controller.notes.length,
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+          staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Get.to(
+                  NoteDetailScreen(),
+                  arguments: index,
+                );
+              },
+              onLongPress: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialogWidget(
+                      contentText: "Are you sure you want to delete the note?",
+                      confirmFunction: () {
+                        controller.deleteNote(controller.notes[index].id);
+                        Get.back();
+                      },
+                      declineFunction: () {
+                        Get.back();
+                      },
+                    );
+                  },
+                );
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.all(15),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      controller.notes[index].title,
+                      style: TextStyle(
+                        fontSize: 21,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      softWrap: false,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      controller.notes[index].content,
+                      style: TextStyle(
+                        fontSize: 17,
+                      ),
+                      maxLines: 6,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      controller.notes[index].dateTimeEdited,
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        brightness: Brightness.light,
+        title: Text(
+          "Notes",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: SearchBar());
+            },
+          ),
+          PopupMenuButton(
+            onSelected: (val) {
+              if (val == 0) {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialogWidget(
+                      contentText: "Are you sure you want to delete all notes?",
+                      confirmFunction: () {
+                        controller.deleteAllNotes();
+                        Get.back();
+                      },
+                      declineFunction: () {
+                        Get.back();
+                      },
+                    );
+                  },
+                );
+              }
+            },
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                value: 0,
+                child: Text(
+                  "Delete All Notes",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+      body: GetBuilder<NoteController>(
+        builder: (_) => controller.isEmpty() ? emptyNotes() : viewNotes(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Get.to(AddNewNotePage());
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+}
